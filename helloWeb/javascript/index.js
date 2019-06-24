04fa8334191eb6508060c949ec29469e27dfe22e
tnovak-olin <tnovak@olin.edu>
2019-06-24 16:55:21 -0400

    created a website which displays the desired functionality though it is not hooked into clew yet and it is unknown how the website interacts with accessability

---
+++ helloWeb/javascript/index.js
@@ -0,0 +1,2 @@
+//gets the element with the id 'date' which is the fourth level heading in our site then it sets the text equal to the date retrieved from the computer
+document.getElementById('date').innerHTML = new Date().toDateString();
